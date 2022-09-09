b1efed68162ced7e7abd32fe29cba20c9f24b88d
Sasu Osayande <osayande.sasu@outlook.com>
2022-09-08 23:52:09 -0400

    Set up Snowpack bundler with TypeScript and set up  boilerplate code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+// import {  v4 as uuidv4 } from "uuid";
 
-import confetti from 'canvas-confetti';
+// console.log(uuidv4());
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector("#list");
